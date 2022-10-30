b127aba31a7c4391ccdabcdb5ffe941eb3349fc0
Justiceshrs C. Portal <justiceshrsportal@gmail.com>
2022-10-30 22:56:13 +0800

    Use kit graphql API

--- lib/omniauth/strategies/snapchat.rb
+++ lib/omniauth/strategies/snapchat.rb
@@ -10,29 +10,37 @@ module OmniAuth
       option :client_options, {
         site: 'https://adsapi.snapchat.com',
         authorize_url: 'https://accounts.snapchat.com/login/oauth2/authorize',
-        token_url: 'https://accounts.snapchat.com/login/oauth2/access_token'
+        token_url: 'https://accounts.snapchat.com/login/oauth2/access_token',
+        auth_scheme: :request_body
       }
 
-      uid { raw_info['me']['id'] }
-
+      uid {
+        raw_info["data"]["me"]["externalId"]
+      }
+      
       info do
         {
-          email: raw_info['me']['email'],
-          organization_id: raw_info['me']['organization_id'],
-          display_name: raw_info['me']['display_name'],
-          member_status: raw_info['me']['member_status']
+          display_name: raw_info['data']['me']['displayName']
         }
       end
-
+      
       extra do
         {
           'raw_info' => raw_info
         }
       end
-
+      
       def raw_info
-        raw_info_url = "https://adsapi.snapchat.com/v1/me"
-        @raw_info ||= access_token.get(raw_info_url).parsed
+        raw_info_url = "https://kit.snapchat.com/v1/me"
+        @raw_info ||= access_token.post(
+          raw_info_url, 
+          mode: :header, 
+          header_format: 'Bearer %s', 
+          body: "{'query':'{me{displayName bitmoji{avatar} externalId}}'}",
+          "Content-Type" => "application/json"
+        ).parsed
+        
+        @raw_info || {'me' => {}}
       end
 
       def callback_url
